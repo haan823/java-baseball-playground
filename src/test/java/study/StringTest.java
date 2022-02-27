b8dfac4dd4b9519645bb44de15245dcd56871665
SANGHYUN KIM <haan823@naver.com>
2022-02-27 12:48:07 +0900

    String Collection에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +12,38 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 테스트")
+    void charAt(){
+        Character actual = "abc".charAt(0);
+        assertThat(actual).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 테스트")
+    void charAt2() {
+        assertThatThrownBy(() -> {
+            Character actual = "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
 }
